b4d94b3e9762d52c3829feb670a23b34ac51d340
Stephan <55607356+stehessel@users.noreply.github.com>
2022-07-25 14:06:43 +0200

    fix cluster overview grafana dashboard (#12)
    Small fix to the cluster overview dashboard. I also fixed the pre-commit
    hook to properly lint the dashboards.

--- scripts/lint-grafana.sh
+++ scripts/lint-grafana.sh
@@ -3,19 +3,16 @@
 set -eu
 
 if ! [ -x "$(command -v dashboard-lint)" ]; then
-  go install github.com/grafana/dashboard-linter@latest
+	go install github.com/grafana/dashboard-linter@latest
 fi
 
-! [ -x "$(command -v yq)" ] && echo 'yq not installed, the hook requires it.' && exit 1;
+! [ -x "$(command -v yq)" ] && echo 'yq not installed, the hook requires it.' && exit 1
 
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)"
 
-TEMP_DIR="$(mktemp -d)"
-
-yq eval '.spec.json' "${SCRIPT_DIR}"/../resources/grafana/rhacs-central-overview-dashboard.yaml > "${TEMP_DIR}"/dashboard.json
-
-cp "${SCRIPT_DIR}"/.lint "${TEMP_DIR}"/.lint
-
-dashboard-linter lint "${TEMP_DIR}"/dashboard.json --strict --verbose
-
-rm -rf "${TEMP_DIR}"
+TMPDIR="${TMPDIR:-/tmp}"
+cp "${SCRIPT_DIR}"/.lint "${TMPDIR}"/.lint
+for f in "${SCRIPT_DIR}"/../resources/grafana/*.yaml; do
+	yq eval '.spec.json' "$f" >"${TMPDIR}"/dashboard.json
+	dashboard-linter lint "${TMPDIR}"/dashboard.json --strict --verbose
+done
