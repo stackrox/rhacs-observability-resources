e7d415c3e0821bcdb86d23a83b0a6e2fb2e5484d
Stephan Heßelmann <shesselm@redhat.com>
2022-07-26 20:48:22 +0200

    first batch of address review comments

--- scripts/test-prom-rules.sh
+++ scripts/test-prom-rules.sh
@@ -8,9 +8,12 @@ set -eu
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)"
 
-for f in "${SCRIPT_DIR}"/../resources/prometheus/unit_tests/*; do
+if [[ ! -f "${SCRIPT_DIR}"/../resources/prometheus/__prometheus-rules-test.yaml ]]; then
 	yq eval '.spec' "${SCRIPT_DIR}"/../resources/prometheus/prometheus-rules.yaml >"${SCRIPT_DIR}"/../resources/prometheus/__prometheus-rules-test.yaml
-	promtool test rules "$f"
+fi
+for var in "$@"; do
+	echo "$var"
+	promtool test rules "${var}"
 done
 
-rm -f "${SCRIPT_DIR}"/../resources/prometheus/__prometheus-rules-test.yaml || true
+# rm -f "${SCRIPT_DIR}"/../resources/prometheus/__prometheus-rules-test.yaml || true
