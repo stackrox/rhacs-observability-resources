d4afab56f5055f4f0fa667916becfbe8f938b514
Vinay Bommana <vbommana@redhat.com>
2023-07-27 12:12:22 +0530

    shellcheck lint error correction

--- resources/grafana/scripts/generate-dashboards.sh
+++ resources/grafana/scripts/generate-dashboards.sh
@@ -4,8 +4,6 @@ set -eu
 
 ! [ -x "$(command -v yq)" ] && echo 'yq not installed, the hook requires it.' && exit 1
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)"
-
 name="rhacs-consolidated-configmap"
 yq ".data.json = ($(cat generated/dashboards/rhacs-consolidated.json) | to_json)" templates/dashboards/"$name".yaml > "$name".yaml
 
