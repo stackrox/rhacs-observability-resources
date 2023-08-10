0657c785103e7ce37777c154b3ee22ec68e296ef
Vinay Bommana <vbommana@redhat.com>
2023-08-10 15:34:11 +0530

    added consolidated dashboards - combining cluster overview and central metrics

--- resources/grafana/scripts/generate-dashboards.sh
+++ resources/grafana/scripts/generate-dashboards.sh
@@ -4,18 +4,13 @@ set -eu
 
 ! [ -x "$(command -v yq)" ] && echo 'yq not installed, the hook requires it.' && exit 1
 
-SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$0}")" &>/dev/null && pwd 2>/dev/null)"
-
-for file in "${SCRIPT_DIR}"/*.json; do
+for file in *.json; do
     name=$(basename "$file" .json)
-    if [ "$name" == "rhacs-central" ]
-    then
-      # in-cluster
-      cat templates/dashboards/"$name"-dashboard.yaml > generated/dashboards/"$name"-dashboard.yaml
-      sed "s/^/    /" "$name".json >> generated/dashboards/"$name"-dashboard.yaml
+    # in-cluster
+    cat templates/dashboards/"$name"-dashboard.yaml > generated/dashboards/"$name"-dashboard.yaml
+    sed "s/^/    /" "$name".json >> generated/dashboards/"$name"-dashboard.yaml
 
-      # cluster-wide
-      cat templates/dashboards/"$name"-configmap.yaml > generated/dashboards/"$name"-configmap.yaml
-      sed "s/^/    /" "$name".json >> generated/dashboards/"$name"-configmap.yaml
-    fi
+    # cluster-wide
+    cat templates/dashboards/"$name"-configmap.yaml > generated/dashboards/"$name"-configmap.yaml
+    sed "s/^/    /" "$name".json >> generated/dashboards/"$name"-configmap.yaml
 done
